3687fbfc99c9c451b446c4172451dadfbb3c7748
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-12 15:30:04 -0300

    feat: Add POST route for user creation

--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+    createUserController,
     findOneUserController,
     listUserController,
 } from "./_controllers/user.controller";
@@ -7,7 +8,12 @@ import {
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/users", listUserController);
+
+app.post("/users", createUserController);
+
 app.get("/users/:userId", findOneUserController);
 
 app.listen(port, () => {
