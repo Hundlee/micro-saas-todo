f2499b4462dbe4e12dad376273b3b4b23917fd06
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-13 12:39:16 -0300

    feat: Configure route to handle Stripe webhook events

--- source/index.ts
+++ source/index.ts
@@ -6,9 +6,16 @@ import {
 } from "./_controllers/user.controller";
 import { createTodoController } from "./_controllers/todo.controller";
 import { createCheckoutController } from "./_controllers/checkout.controller";
+import { stripeWebhookController } from "./_controllers/stripe.controller";
 
 const app = express();
 const port = 3000;
+
+app.post(
+    "/stripe",
+    express.raw({ type: "application/json" }),
+    stripeWebhookController
+);
 
 app.use(express.json());
 
