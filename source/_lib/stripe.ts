7b04afe08fb6c1394b241423909594900df4e350
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-13 12:35:36 -0300

    feat: Add Stripe customer retrieval and creation functions

--- source/_lib/stripe.ts
+++ source/_lib/stripe.ts
@@ -4,14 +4,38 @@ import { prisma } from "./prisma";
 
 export const stripe = new Stripe(config.stripe.secretKey, {
     apiVersion: "2024-04-10",
+    httpClient: Stripe.createFetchHttpClient(),
 });
 
-export const createCheckoutSession = async (userId: string) => {
+export const getStripeCustomerByEmail = async (email: string) => {
+    const customers = await stripe.customers.list({ email });
+    return customers.data[0];
+};
+
+export const createStripeCustomer = async (input: {
+    name?: string;
+    email: string;
+}) => {
+    let customer = await getStripeCustomerByEmail(input.email);
+    if (customer) return customer;
+
+    return stripe.customers.create({ email: input.email, name: input.name });
+};
+
+export const createCheckoutSession = async (
+    userId: string,
+    userEmail: string
+) => {
     try {
+        let customer = await createStripeCustomer({
+            email: userEmail,
+        });
+
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ["card"],
             mode: "subscription",
             client_reference_id: userId,
+            customer: customer.id,
             success_url: `http://localhost:3000/success.html`,
             cancel_url: `http://localhost:3000/cancel.html`,
             line_items: [
@@ -26,7 +50,7 @@ export const createCheckoutSession = async (userId: string) => {
             url: session.url,
         };
     } catch (error) {
-        console.error(error);
+        throw new Error(`Error to create checkout session`);
     }
 };
 export const handleProcessWebhookCheckout = async (event: {
